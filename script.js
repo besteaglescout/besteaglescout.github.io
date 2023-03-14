04da2140ea5ab77fac03f8bf168dba19229df54f
besteaglescout <127067897+besteaglescout@users.noreply.github.com>
2023-03-14 08:23:39 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 function onready(){
     var y = window.innerHeight
-    //console.log(document.getElementById("buttons").style.height)
     var x = window.innerWidth
     document.getElementById("buttons").style.top = (y-document.getElementById("buttons").offsetHeight)
     document.getElementById("buttons").style.left = (x/2-(document.getElementById("buttons").offsetWidth/2))
 }
 
 function towhat(){
+    document.getElementById("sign").style.background = ""
+    document.getElementById("when").style.background = ""
     location.hash = "what"
+    document.getElementById("what").style.background = "yellow"
+}
+
+function towhen(){
+    document.getElementById("sign").style.background = ""
+    document.getElementById("what").style.background = ""
+    location.hash = "when"
+    document.getElementById("when").style.background = "yellow"
+}
+
+function tosign(){
+    document.getElementById("what").style.background = ""
+    document.getElementById("when").style.background = ""
+    location.hash = "sign"
+    document.getElementById("sign").style.background = "yellow"
 }
