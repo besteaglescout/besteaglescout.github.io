de6f668352b9ef516eb4aa12be4bddd19d9d4dbf
besteaglescout <127067897+besteaglescout@users.noreply.github.com>
2023-03-14 09:08:04 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function onready(){
-    var y = window.innerHeight
+    const jsConfetti = new JSConfetti()
+    once = true
+    jsConfetti.addConfetti({
+        emojis: ['👍'],
+        emojiSize: 100,
+        confettiNumber: 60,
+        })
+        setTimeout(function(){
+            if (window.innerHeight > window.innerWidth){
+                window.alert("Please turn your screen horizontally so it is better to see my website.")
+                myInt = setInterval(function(){
+                    if (window.innerHeight < window.innerWidth){
+                        clearInterval(myInt)
+                        window.alert("Thanks")
+                    }
+                }, 1000)
+            }
+
+        }, 8000)
+    /*var y = window.innerHeight
     var x = window.innerWidth
     document.getElementById("buttons").style.top = (y-document.getElementById("buttons").offsetHeight)
-    document.getElementById("buttons").style.left = (x/2-(document.getElementById("buttons").offsetWidth/2))
+    document.getElementById("buttons").style.left = (x/2-(document.getElementById("buttons").offsetWidth/2))*/
 }
 
 function towhat(){
@@ -25,3 +44,4 @@ function tosign(){
     location.hash = "sign"
     document.getElementById("sign").style.background = "yellow"
 }
+
